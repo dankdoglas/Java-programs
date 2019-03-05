210583804217cc9d3d07ee296d3085656d5d334f
Yisheng Toh <42954321+dankdoglas@users.noreply.github.com>
2019-03-05 20:25:28 +0800

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class factorial {
+	private static Scanner sc;
+	private static int a, b, c, d;
+	public static void main(String[] args) {
+		System.out.println("Find the Factorial(!) of:");
+		sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = 1;
+			if (a ==  1 && a == 0) {
+				System.out.println(1);
+			}
+		
+			else {
+				for (int i = 1; i <= a; i++) {
+					b *= i;
+					
+				}
+				System.out.print(b);
+			}
+	}
+
+}
