903b1e7da966fa59df1d72e17a4d6d1b80cee620
Yisheng Toh <42954321+dankdoglas@users.noreply.github.com>
2019-03-05 19:20:32 +0800

    Update QuadraticEquations.java

--- QuadraticEquations.java
+++ QuadraticEquations.java
@@ -1,42 +1,34 @@
 
-class QuadraticEquations {
-	
-	double a = 1.0, b = -4.0, c = 0.0;
-	double Mid = -b/(2*a);
-	double Vertex = (a)*(Mid*Mid) + (b*Mid) + c;
-	double Root = (b*b)-(4.0*a*c);
-	double QuadFormula1 = (b*-1.0 + Math.sqrt(Root))/(2*a);
-	double QuadFormula2 = (b*-1.0 - Math.sqrt(Root))/(2*a);
-	//Change a, b, c according to Equation
-	
-	public void Discriminant() {
-		if (b*b-(4.0*a*c) < 0)
-		{ System.out.println("The parabola have no x intercepts");}
-		else if (b*b-4.0*a*c > 0)
-		{ System.out.println("The parabola have two x intercepts");}
-		else if (b*b-4.0*a*c == 0)
-		{ System.out.println("The parabola have one intercepts");}}
-	
-	public void midpoint() {
-		System.out.println("The mid point of the Quadratic is " +  Mid);}
-	
-	public void vertex() {
-		if (a > 0)
-		System.out.println("The Minimum point of Parabola is " + Vertex);
-		else if (a < 0)
-		System.out.println("The Maximum point of Parabola is " + Vertex);}
-	
-	public void QuadFormula() {
-		System.out.println("x equal to " + QuadFormula1 + " or x equal to " + QuadFormula2);}
+import java.util.Scanner;
 
-	public void yintercept() {
-		System.out.println("The y-intercept is " + c );}
-	
+public class quadratic {
+	private static Scanner sc;
+
 	public static void main(String[] args) {
-		QuadraticEquation find = new QuadraticEquation();
-		//call the operation using find."operation"
-		find.yintercept();
-		find.QuadFormula();
-		find.midpoint();
-		find.vertex();
-		find.Discriminant();
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		double c = sc.nextInt();
+		double Mid = -b/(2*a);
+		double Vertex = a*(Mid*Mid) + (b*Mid) + c;
+		double Root = (b*b) - (4.0 * a * c);
+		double QuadFormula1 = (b *-1.0 + Math.sqrt(Root))/(2*a);
+		double QuadFormula2 = (b *-1.0 - Math.sqrt(Root))/(2*a);	
+		
+			System.out.println("The y-intercept is " + c );
+			System.out.println("x equal to " + QuadFormula1 + " or x equal to " + QuadFormula2);
+			System.out.println("The mid point of the Quadratic is " +  Mid);
+	
+			if (a > 0) {
+				System.out.println("The Minimum point of Parabola is " + Vertex);}
+			else if (a < 0) {
+				System.out.println("The Maximum point of Parabola is " + Vertex);}
+			else if (b*b-(4.0*a*c) < 0){ 
+				System.out.println("The parabola have no x intercepts");}
+			else if (b*b-4.0*a*c > 0){
+				System.out.println("The parabola have two x intercepts");}
+			else if (b*b - 4.0*a*c == 0){
+				System.out.println("The parabola have one intercepts");}
+		}
+}
+
